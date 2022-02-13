27f8fa8824ee8383453da9e466480702138acf2d
Jeremy Pena <krillik08@gmail.com>
2022-02-12 20:36:58 -0600

    add array and update for loop

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,35 @@
-
 const container = document.getElementById('container');
+const cardData = [
+    {
+        title: 'Title 1',
+        text: 'Some quick example text to build on the card title \
+        and make up the bulk of the card\'s content.'
+    },
+    {
+        title: 'Title 2',
+        text: 'Some quick example text to build on the card title \
+        and make up the bulk of the card\'s content.'
+    },
+    {
+        title: 'Title 3',
+        text: 'Some quick example text to build on the card title \
+        and make up the bulk of the card\'s content.'
+    },
+    {
+        title: 'Title 4',
+        text: 'Some quick example text to build on the card title \
+        and make up the bulk of the card\'s content.'
+    },
+]
 
-const addCard = () => {
+const addCard = (txtTitle, txtDescription) => {
     let card = document.createElement('div')
     let spacer = document.createElement('br')
     card.className = 'card'
     card.style.width = '18rem'
     card.innerHTML = '<div class="card-body"> \
-        <h5 class="card-title">Card title</h5> \
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p> \
+        <h5 class="card-title">' + txtTitle + '</h5> \
+        <p class="card-text">' + txtDescription + '</p> \
         <a href="#" class="btn btn-primary">Go somewhere</a> \
         </div > \
         </div >'
@@ -16,6 +37,6 @@ const addCard = () => {
     container.appendChild(spacer);
 }
 
-for (let i = 0; i < 4; i++) {
-    addCard();
+for (const entry of cardData) {
+    addCard(entry.title, entry.text)
 }
